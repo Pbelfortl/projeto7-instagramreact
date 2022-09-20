d2540d42546f03e74fe8014070a86884bf901dc8
Pablo Belfort <pablobelfort@gmail.com>
2022-09-20 02:08:07 -0300

    versao final

--- src/Posts.js
+++ src/Posts.js
@@ -1,53 +1,5 @@
 import react, { useState } from "react"
 
-
-function Post(p) {
-
-    const [bookmark, setBookmark] = react.useState("bookmark-outline")
-    const [heart, setHeart] = react.useState("heart-outline")
-
- //   const post = postsArr.map((p) =>
-    return (
-        <div class="post">
-            <div class="topo">
-                <div class="usuario">
-                    <img src={p.imagem} />
-                    {p.usuario}
-                </div>
-                <div class="acoes">
-                    <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-            </div>
-
-            <div class="conteudo">
-                <img src={p.conteudo} onClick={() => setHeart((heart === "heart-outline") ? "heart" : "heart-outline")} />
-            </div>
-
-            <div class="fundo">
-                <div class="acoes">
-                    <div>
-                        <ion-icon name={heart} onClick={() => setHeart((heart === "heart-outline") ? "heart" : "heart-outline")}></ion-icon>
-                        <ion-icon name="chatbubble-outline"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                    </div>
-                    <div>
-                        <ion-icon name={bookmark} onClick={() => setBookmark((bookmark === "bookmark-outline") ? "bookmark" : "bookmark-outline")} ></ion-icon>
-                    </div>
-                </div>
-
-                <div class="curtidas">
-                    <img src="assets/img/respondeai.svg" />
-                    <div class="texto">
-                        Curtido por <strong>{p.curtiu}</strong> e <strong>outras {p.curtidas} pessoas</strong>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    )
-    
-}
-
 export default function posts() {
 
     return (
@@ -63,6 +15,65 @@ export default function posts() {
             ))}
         </div>
     )
+}
+
+function Post(p) {
+
+    const [bookmark, setBookmark] = react.useState("bookmark-outline")
+    const [heart, setHeart] = react.useState("heart-outline")
+    const [curtidas, setCurtidas] = react.useState(p.curtidas)
+
+    function curtiu () {
+    
+        if(heart === "heart-outline"){
+
+            setHeart("heart")
+            setCurtidas(curtidas+1)
+
+        }  else {setHeart("heart-outline") 
+                setCurtidas(curtidas-1)}
+       
+    }
+
+    return (
+        <div class="post">
+            <div class="topo">
+                <div class="usuario">
+                    <img src={p.imagem} />
+                    {p.usuario}
+                </div>
+                <div class="acoes">
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+            </div>
+
+            <div class="conteudo">
+                <img src={p.conteudo} onClick={curtiu} />
+            </div>
+
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                        <ion-icon name={heart} onClick={curtiu}></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                    </div>
+                    <div>
+                        <ion-icon name={bookmark} onClick={() => setBookmark((bookmark === "bookmark-outline") ? "bookmark" : "bookmark-outline")} ></ion-icon>
+                    </div>
+                </div>
+
+                <div class="curtidas">
+                    <img src="assets/img/respondeai.svg" />
+                    <div class="texto">
+                        Curtido por <strong>{p.curtiu}</strong> e <strong>outras {curtidas} pessoas</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+    
 }
 
 const postsArr = [
